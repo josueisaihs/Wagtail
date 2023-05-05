5a838446f9e0be209d49366e287fcd58256d1856
josueisaihs <josueisaihs@gmail.com>
2023-05-05 08:45:11 +0100

    Add basic homepage

--- src/library/home/models.py
+++ src/library/home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    header = models.CharField(max_length=255, default='Header')
+    subheader = models.CharField(max_length=255, default='Subheader')
+    image = models.ForeignKey("wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+")
+    emphasis_text = models.CharField(max_length=255, default='Emphasis text')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('header'),
+        FieldPanel('subheader'),
+        FieldPanel('image'),
+        FieldPanel('emphasis_text'),
+    ]
